048bbde790b09632d1136848634365164c80be72
Diego Veiga <sdiegoveiga@gmail.com>
2024-03-07 08:33:46 -0300

    initial config, eslint, designsystem and clean project

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,8 @@
+import { globalStyle } from '@/styles/global'
+import type { AppProps } from 'next/app'
+
+globalStyle()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
