4f32b5692d3fd274d37113d49d566f18ad60097b
Diego Veiga <sdiegoveiga@gmail.com>
2024-03-07 21:16:03 -0300

    ClaimUsernameForm component

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,20 @@
+import { Button, TextInput } from '@ignite-ui/react'
+import { Form } from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export function ClaimUsernameForm() {
+  return (
+    <Form as="form">
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        crossOrigin=""
+        placeholder="seu-usuário"
+      />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  )
+}
